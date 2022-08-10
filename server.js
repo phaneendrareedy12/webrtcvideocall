edc23adae02cd41ea24ab2b5757af00ac178ce30
phaneeendrareddy12 <phaneendra.katta1@gmail.com>
2022-08-10 21:15:49 +0530

    webrtc cassandra

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require('express')
 //Socket.IO is a library that enables communication between a client and a server.
 const { Socket } = require('socket.io')
 // mongoose library will be used to connect to Monogo DB
-const mongoose = require('mongoose')
+//const mongoose = require('mongoose')
 // axios library will be used cosume rest calls (like GET, POST, PUT etc..)
 const axios = require('axios')
 // to genetae unique id on every peer communication, we will replace device id in future
 const uuid = require('react-uuid')
+//Cassandra driver
+const cassandra = require('cassandra-driver')
 
 //configure socket io, same path should be on client as well
 const io = require('socket.io')({
@@ -21,71 +23,92 @@ const port = 8081 // Siganling server will run on this port
 //Get app to test if our server is up or not , This api doen't have any purpose other than checking whether server is up or not
 app.get('/', (req, res) => res.send('Hello WebRTC!!'))
 
-//Mongo DB connection
-mongoose.connect('mongodb://localhost:27017/webrtc', {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}, err => {
+//Connect with cassandra
+const authProvider = new cassandra.auth.PlainTextAuthProvider('cassandra', 'cassandra')
+const client = new cassandra.Client({ contactPoints: ['192.168.209.67:9042'], localDataCenter: 'datacenter1', authProvider: authProvider})
+client.connect(err => {
     if(!err) {
-        console.log('connected to mongo db ........')
+        console.log('connected to cassandra db ........')
     } else {
-        console.log('error connnecting mongo db....')
+        console.log('error connnecting cassandra db....', err)
     }
-})
+});
 
-//Javascript object which will be attached mandoDB model to save offer and answer details
-const offerAnswerSchema = {
-    type:String,
-    sdp:String,
-    timestamp:Date,
-    deviceid:String
-}
+//Mongo DB connection
+// mongoose.connect('mongodb://localhost:27017/webrtc', {
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// }, err => {
+//     if(!err) {
+//         console.log('connected to mongo db ........')
+//     } else {
+//         console.log('error connnecting mongo db....')
+//     }
+// })
+
+// //Javascript object which will be attached mandoDB model to save offer and answer details
+// const offerAnswerSchema = {
+//     type:String,
+//     sdp:String,
+//     timestamp:Date,
+//     deviceid:String
+// }
 
 
-const monModelOfferAnswer = mongoose.model('signalingdata',offerAnswerSchema)
+//const monModelOfferAnswer = mongoose.model('signalingdata',offerAnswerSchema)
 
 // Post API to save offer and answer details to MongoDB
 app.post('/saveOfferAnswer', async(req,res) => {
 
-    const dataToSave = new monModelOfferAnswer({
-        type:req.body.sdp.type,
-        sdp:req.body.sdp.sdp,
-        timestamp:new Date(),
-        deviceid:uuid()
-    })
+    const query = 'INSERT INTO webrtc_data.webrtc_sdp (id, type, sdp, time_stamp) VALUES (?, ?, ?, toTimestamp(now()))'
+    const params = [uuid(), req.body.sdp.type, req.body.sdp.sdp]
+    client.execute(query, params, { prepare: true }, err => {
+        if(!err) {
+            console.log('sdp data inserted into webrtc_data.webrtc_sdp ........')
+        } else {
+            console.log('error inserted sdp data into webrtc_data.webrtc_sdp ........', err)
+        }
+      })
 
-    const val = await dataToSave.save()
-    console.log('res josn ***')
-    console.log(val)
-    res.json(val)
+    // const dataToSave = new monModelOfferAnswer({
+    //     type:req.body.sdp.type,
+    //     sdp:req.body.sdp.sdp,
+    //     timestamp:new Date(),
+    //     deviceid:uuid()
+    // })
+
+    // const val = await dataToSave.save()
+    // console.log('res josn ***')
+    // console.log(val)
+    // res.json(val)
 })
 
 //Javascript object which will be attached mandoDB model to save ice candidates
-const iceCandidatesSchema = {
-    candidate:String,
-    sdpMid:String,
-    sdpMLineIndex:String,
-    usernameFragment:String,
-    timestamp:Date
-}
+// const iceCandidatesSchema = {
+//     candidate:String,
+//     sdpMid:String,
+//     sdpMLineIndex:String,
+//     usernameFragment:String,
+//     timestamp:Date
+// }
 
-const monIceCandidates = mongoose.model('iceCandidates',iceCandidatesSchema)
+//const monIceCandidates = mongoose.model('iceCandidates',iceCandidatesSchema)
 
 // Post API to save ice candidates details to MongoDB
 app.post('/saveIceCandidates', async(req,res) => {
 
-    const dataToSave = new monIceCandidates({
-        candidate:req.body.candidate,
-        sdpMid:req.body.sdpMid,
-        sdpMLineIndex:req.body.sdpMLineIndex,
-        usernameFragment:req.body.usernameFragment,
-        timestamp:new Date()
-    })
+    // const dataToSave = new monIceCandidates({
+    //     candidate:req.body.candidate,
+    //     sdpMid:req.body.sdpMid,
+    //     sdpMLineIndex:req.body.sdpMLineIndex,
+    //     usernameFragment:req.body.usernameFragment,
+    //     timestamp:new Date()
+    // })
 
-    const val = await dataToSave.save()
-    console.log('res josn ***')
-    console.log(val)
-    res.json(val)
+    // const val = await dataToSave.save()
+    // console.log('res josn ***')
+    // console.log(val)
+    // res.json(val)
 })
 
 const sever = app.listen(port, () => {
@@ -120,12 +143,12 @@ webRTCNamespace.on('connection', socket => {
         socket.broadcast.emit('sdp', data)
 
         //Call post /saveOfferAnswer api, Save the data to DB 
-        axios.post('http://localhost:8080/saveOfferAnswer', data)
+        axios.post('http://localhost:8081/saveOfferAnswer', data)
         .then(response => {
             console.log("post called successfully")
         })
         .catch(error => {
-            console.log("error occurred while calling post method which saves answer offer data into DB")
+            console.log("error occurred while calling post method which saves answer offer data into DB", error)
         }) 
     }) 
 
@@ -135,12 +158,12 @@ webRTCNamespace.on('connection', socket => {
         socket.broadcast.emit('candidate', data)
 
         //Call post /saveIceCandidates api, Save the data to DB 
-        axios.post('http://localhost:8080/saveIceCandidates', data)
-        .then(response => {
-            console.log("post called successfully")
-        })
-        .catch(error => {
-            console.log("error occurred while calling post method which saves ice candidates data into DB")
-        }) 
+        // axios.post('http://localhost:8081/saveIceCandidates', data)
+        // .then(response => {
+        //     console.log("post called successfully")
+        // })
+        // .catch(error => {
+        //     console.log("error occurred while calling post method which saves ice candidates data into DB")
+        // }) 
     })
 })
